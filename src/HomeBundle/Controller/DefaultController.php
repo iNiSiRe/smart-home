d5e9cf51639518a9b752cd9ae0065f43255574d7
inisire <inisire@gmail.com>
2018-09-20 14:30:20 -0400

    Improvements

--- src/HomeBundle/Controller/DefaultController.php
+++ src/HomeBundle/Controller/DefaultController.php
@@ -2,13 +2,9 @@
 
 namespace HomeBundle\Controller;
 
-use HomeBundle\Listener\TestListener;
-use inisire\ReactBundle\EventDispatcher\AsynchronousEventDispatcher;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -43,18 +39,6 @@ class DefaultController extends Controller
      */
     public function messageAction(Request $request)
     {
-        $this->get('logger')->debug('controller 1');
-
-        $rooms = $this->get('doctrine.orm.entity_manager')->getRepository('HomeBundle:Room')->findAll();
-
-        $dispatcher = $this->container->get(AsynchronousEventDispatcher::class);
-
-        $this->get('logger')->debug('controller 2');
-
-        $dispatcher->dispatch('test', new Event());
-
-        $this->get('logger')->debug('controller 3');
-
-        return new JsonResponse(['success' => true, 'class' => get_class($this->container->get('react.loop')), 'rooms' => count($rooms)]);
+        return new JsonResponse(['success' => true]);
     }
 }
