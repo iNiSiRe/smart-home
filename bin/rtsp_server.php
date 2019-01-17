aa6d3945297b73bc4344b84160f6b3e6ab46e30a
inisire <inisire@gmail.com>
2019-01-17 15:25:30 +0200

    Fix rtsp

--- bin/rtsp_server.php
+++ bin/rtsp_server.php
@@ -5,13 +5,13 @@ $loader = require __DIR__ . '/../app/autoload.php';
 $loop = React\EventLoop\Factory::create();
 
 $ffmpeg = new \React\ChildProcess\Process('ffmpeg -i "rtsp://admin:ju789lki@192.168.31.197:554/onvif1" -f mpjpeg pipe:');
+
+$stream = new \React\Stream\ThroughStream();
+$ffmpeg->stdout->pipe($stream);
+
 $ffmpeg->start($loop);
 
-$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) use ($ffmpeg) {
-
-    $stream = new \React\Stream\ThroughStream();
-
-    $ffmpeg->stdout->pipe($stream);
+$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) use ($stream) {
 
     return new React\Http\Response(
         200,
