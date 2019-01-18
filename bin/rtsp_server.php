40c3d2061c57525ef563d0f4f7cd7eeea57ec46f
inisire <inisire@gmail.com>
2019-01-18 22:11:38 +0200

    Fix dependencies

--- bin/rtsp_server.php
+++ bin/rtsp_server.php
@@ -7,31 +7,31 @@ $loop = React\EventLoop\Factory::create();
 $ffmpeg = new \React\ChildProcess\Process('ffmpeg -i "rtsp://admin:ju789lki@192.168.31.197:554/onvif1" -f mpjpeg pipe:');
 $ffmpeg->start($loop);
 
-$ffmpeg->on('exit', function () {
-//    print 'ffmpeg exit' . PHP_EOL;
-});
-
 $received = 0;
 $working = false;
 
 $handle = fopen('dev.log', 'a');
 $file = new \React\Stream\WritableResourceStream($handle, $loop);
 
-$loop->addPeriodicTimer(1, function () use (&$received, &$working) {
+$ffmpeg->on('exit', function () use ($file) {
+    $file->write('ffmpeg exit' . PHP_EOL);
+});
 
+$loop->addPeriodicTimer(1, function () use (&$received, &$working, $file) {
+
+    $file->write($received . PHP_EOL);
     $working = $received > 0;
     $received = 0;
 
 });
 
-$ffmpeg->stdout->on('data', function ($chunk) use (&$received, $file) {
-
+$ffmpeg->stdout->on('data', function ($chunk) use (&$received) {
     $received += strlen($chunk);
-    $file->write($received . PHP_EOL);
-
 });
 
-$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) use ($ffmpeg, $loop, &$working) {
+$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) use ($ffmpeg, $loop, &$working, $file) {
+
+    $file->write('request' . PHP_EOL);
 
     if ($request->getUri()->getPath() == '/status') {
 
@@ -66,8 +66,8 @@ $server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterfac
     }
 });
 
-$server->on('error', function ($error) {
-    var_dump($error);
+$server->on('error', function ($error) use ($file) {
+    $file->write('error ' . get_class($error) . PHP_EOL);
 });
 
 $socket = new React\Socket\Server('0.0.0.0:9001', $loop);
